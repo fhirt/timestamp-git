bd08ccc71ab0843964be6477af72d47ca6477513
fhirt <fabio_hirt@hotmail.com>
2017-05-30 19:54:08 +0000

    deploy heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const IP = 80;
+const IP = process.env.PORT || 8080;
 
 var answer = {
     "unix": "unixdate",
